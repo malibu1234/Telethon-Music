0a25ff6b393428e460848965e579d9dcae1c70b4
malibu1234 <108977636+malibu1234@users.noreply.github.com>
2023-01-02 02:33:54 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "25302801"))
     API_HASH = os.environ.get("API_HASH", "955818e2e0fa518ca8ab87bdd280b5b4")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5688442859:AAHcUUw9VhqIr5XIYleFcEYcFgF7QlkI3wE ")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BQCFVX-4cBsqg5srtn9RO0XMUYEYIepPl6t6m1rxMzEtRLy-Mu2K0j1Q7Y7yrbxL3hJIbj77T06OysHoOICZVg3Gj-so-wqu272QQyKxg_Og2-ohMyLhbgjwZiynMyNw8zrRNU5AfSfFeevwpu1tUOHatLgkivLolseE-0eFf6gR32spLCL3tXMX8vEM4_j5S8JvOSscrm_4eOC-KC9EHw8AOy9QdEJw0HC_JCBTslVL5HK_UjU71-pLwuhh2V7GYv0k2-wMR2H91NwZIJxJLKpnWUqGgGkvwjnkSiZo39OouOj8qDExM--SC7W--_0ETwQbsmZzNR35-DK30nJdApHVAAAAAWFUaEQA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKkBu2KSuV0dSTZ0jUMgSTYKGlfSR7bNUTIl4DmoLhY9ZFcWytuV094RaJSAEjWdW2nR4tZW3gL02PIQZdvCUnmhS5mLNbDLFWsO_wyTDXJTkpApiloQfv2vHcLZ-h7u35g8hmh89XSw-kaoXHrvvv2fStHBywdm0gRj44SqJxP7n6B31v_KiE6n1l6WZcStiPSl24CNu8nKJS_yniEDcOp8CNsV8DPS-34HNdwnleiZVU7w1_Z6FahiqqVu2wHdjZh1FJR8_gZBdyX52wGngBWr-cMsr2GzV6zrPJVLwxCKNS7BrtNN4MScCZ0rsSOxaYYaiZKWt0xcMieam0T7cOqmT4w=")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "jimm45bot")
@@ -13,4 +13,4 @@ class Config(object):
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/138ef6b5944b8342159dd.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/138ef6b5944b8342159dd.png")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5927888964"))
-    MONGO_DB_URI = mongodb+srv://manu:Rokiputha.90@cluster0.myho7dn.mongodb.net/?retryWrites=true&w=majority
+    
