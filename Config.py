1d702af7b6eb528df1a686841a2dd6a0c279d9c7
malibu1234 <108977636+malibu1234@users.noreply.github.com>
2023-01-02 02:21:49 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,15 +1,16 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    API_ID = int(os.environ.get("APP_ID", "25302801"))
+    API_HASH = os.environ.get("API_HASH", "955818e2e0fa518ca8ab87bdd280b5b4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5688442859:AAHcUUw9VhqIr5XIYleFcEYcFgF7QlkI3wE ")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "BQCFVX-4cBsqg5srtn9RO0XMUYEYIepPl6t6m1rxMzEtRLy-Mu2K0j1Q7Y7yrbxL3hJIbj77T06OysHoOICZVg3Gj-so-wqu272QQyKxg_Og2-ohMyLhbgjwZiynMyNw8zrRNU5AfSfFeevwpu1tUOHatLgkivLolseE-0eFf6gR32spLCL3tXMX8vEM4_j5S8JvOSscrm_4eOC-KC9EHw8AOy9QdEJw0HC_JCBTslVL5HK_UjU71-pLwuhh2V7GYv0k2-wMR2H91NwZIJxJLKpnWUqGgGkvwjnkSiZo39OouOj8qDExM--SC7W--_0ETwQbsmZzNR35-DK30nJdApHVAAAAAWFUaEQA")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Zaid2_Robot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "jimm45bot")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "6435225"))
+    CHANNEL = os.environ.get("CHANNEL", "jimmy music")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/138ef6b5944b8342159dd.png")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/138ef6b5944b8342159dd.png")
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5927888964"))
+    MONGO_DB_URI = mongodb+srv://manu:Rokiputha.90@cluster0.myho7dn.mongodb.net/?retryWrites=true&w=majority
